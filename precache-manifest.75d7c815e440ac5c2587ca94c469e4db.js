42d36fd13813636d6406811eb94e93ea9e3d2b7d
georges guelton <georges.guelton@gmail.com>
2019-07-16 15:57:13 +0200

    Updates

---
+++ precache-manifest.75d7c815e440ac5c2587ca94c469e4db.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20658e36672bd84d0747df7eb32ac061",
+    "url": "/portofio-react/index.html"
+  },
+  {
+    "revision": "43494bf7557e85935649",
+    "url": "/portofio-react/static/css/2.2e7eb412.chunk.css"
+  },
+  {
+    "revision": "2ed8e279a5e6776bc2dd",
+    "url": "/portofio-react/static/css/main.00b04eba.chunk.css"
+  },
+  {
+    "revision": "43494bf7557e85935649",
+    "url": "/portofio-react/static/js/2.00ff6c8d.chunk.js"
+  },
+  {
+    "revision": "2ed8e279a5e6776bc2dd",
+    "url": "/portofio-react/static/js/main.80ce77b0.chunk.js"
+  },
+  {
+    "revision": "c6a9fae2bf1b88efc533",
+    "url": "/portofio-react/static/js/runtime~main.a13b7c29.js"
+  },
+  {
+    "revision": "294676eafabbbc181fab71b168ff61d7",
+    "url": "/portofio-react/static/media/aboutImg.294676ea.jpg"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/portofio-react/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/portofio-react/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/portofio-react/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/portofio-react/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/portofio-react/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/portofio-react/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/portofio-react/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/portofio-react/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/portofio-react/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/portofio-react/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/portofio-react/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/portofio-react/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/portofio-react/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/portofio-react/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/portofio-react/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "d5a2eedb46a9b4f7944c383f13c2ad98",
+    "url": "/portofio-react/static/media/headerbg2.d5a2eedb.jpg"
+  },
+  {
+    "revision": "3e9ec0c53ede7a4910d1bae93e18c521",
+    "url": "/portofio-react/static/media/site1.3e9ec0c5.png"
+  },
+  {
+    "revision": "962db33490e84a534e0cc684e7878e59",
+    "url": "/portofio-react/static/media/site2.962db334.png"
+  },
+  {
+    "revision": "0eb552028b53074795a505253f63369f",
+    "url": "/portofio-react/static/media/site3.0eb55202.png"
+  },
+  {
+    "revision": "ada9c99be79c0302a5ee9f37e883cbb7",
+    "url": "/portofio-react/static/media/site4.ada9c99b.png"
+  },
+  {
+    "revision": "2774e8ff4d7b8636a508d249d989a690",
+    "url": "/portofio-react/static/media/site5.2774e8ff.png"
+  },
+  {
+    "revision": "356bf7dca30afe69b116ba87767f2f3a",
+    "url": "/portofio-react/static/media/site6.356bf7dc.png"
+  }
+]);
